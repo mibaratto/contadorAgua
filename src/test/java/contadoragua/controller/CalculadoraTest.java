bb0d69a92ee98ef232f972867788fe2a78790ed3
Michelle Lang Baratto <mi.baratto@gmail.com>
2017-06-19 21:46:46 -0300

    adiciona teste unitario para a calculadora

--- src/test/java/contadoragua/controller/CalculadoraTest.java
+++ src/test/java/contadoragua/controller/CalculadoraTest.java
@@ -1,13 +1,37 @@
 package contadoragua.controller;
 
 
+import contadoragua.model.Valores;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
-    @Test
-    public void calculaPrecoAgua() throws Exception {
+    Calculadora calculadora;
 
+    @Before
+    public
+    void setUp() {
+        double precoAgua = 1;
+        double tarifaBasica = 10;
+        int mesAtual = 2;
+        int mesAnterior = 1;
+        Valores valores = new Valores(precoAgua, tarifaBasica, mesAtual, mesAnterior);
+        calculadora = new Calculadora(valores);
+    }
+
+    @Test
+    public void deveCalcularPrecoAgua() throws Exception {
+
+        // given
+        double precoEsperado = 11.7;
+
+        // when
+        double preco = calculadora.calculaPrecoAgua();
+
+        // then
+        Assert.assertEquals(precoEsperado, preco, 0);
 
     }
 
